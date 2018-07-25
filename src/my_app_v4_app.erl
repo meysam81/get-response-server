8633b33856b7e6629db7da5464e323ce1b89a95c
Meysam Azad <MeysamAzad81@yahoo.com>
2018-07-25 16:39:02 +0430

    finished the app that implements the application behavior

--- src/my_app_v4_app.erl
+++ src/my_app_v4_app.erl
@@ -2,6 +2,9 @@
 
 -behaviour(application).
 
+-include("my_app_v4.hrl").
+-include("my_app_v4_sample_func.hrl").
+
 %% Application callbacks
 -export([start/2, stop/1]).
 
@@ -10,6 +13,22 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    my_app_v4_dict:init(), %% dictionary initialization
+
+    Port = my_app_v4_utils:get(port),
+
+    ok = application:ensure_started(tecipe),
+
+    {ok, _} = tecipe:start_listener(tecipe_ref,
+                                    Port,
+                                    {my_app_v4_worker, start, []},
+                                    [{monitor, true},
+                                     {pool, 8}],
+                                    [binary,
+                                     {keepalive, false},
+                                     {backlog, 256},
+                                     {reuseaddr, true}]),
+
 
     my_app_v4_sup:start_link().
 
